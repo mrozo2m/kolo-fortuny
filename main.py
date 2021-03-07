3f39c2282289e19abdc09bc8f67a4eceb09c8236
mrozo2m <mrozo2m@interia.pl>
2021-03-07 14:21:52 +0100

    Added whell

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-"""print("Podaj kategorię hasła")
-category = input()
-print("Podaj hasło")
-password = input()"""
-category = "marka samochodu"
-password = "mercedes"
+import random
+import time
 
+category = input('Podaj kategorię hasła ')
+password = input('Podaj hasło \n\n\n\n\n')
+
+whell = [100, 150, 200, 250, 300, 350, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 5000, 'Bankrut', 'Bankrut']
+player_points = 0
 password_blind = ''
 for i in range (0,len(password)):
     password_blind = password_blind + chr(95)
 
 while password != password_blind:
+    print('\n\n\n')
     print('==========================================================')
-    print("Hasło do odgadnięcia", password_blind, "Kategori", category)
+    print("Kategoria:", category, '  Punkty:', player_points)
+    print("Hasło do odgadnięcia:", password_blind)
+    print('==========================================================')
+    draw = random.choice(whell)
+    print('\nKręcisz kołem ...')
+    time.sleep(2)
+    print('koło zatrzymało się na:', draw)
+    if draw == 'Bankrut':
+        player_points = 0
+        continue
     j = 0
     hit = 0
     print('Podaj literę z hasła lub odgaduj hasło')
     quess = input()
     if quess == password:
         print('Brawo ! Odgadnięte hasło to:', password)
+        player_points = player_points + draw
         break
     else:
         for i in password:
             j = j + 1
             if quess == i:
                 password_blind = password_blind[:j-1] + quess + password_blind[j:]
+                player_points = player_points + draw
                 hit = 1
     if hit > 0:
         print("Tarfiłeś")
@@ -31,7 +44,7 @@ while password != password_blind:
         print('Pudło')
 
 print('Brawo ! Odgadnięte hasło to:', password)
-
+print('Twój wynik to:', player_points)
 
 
 
