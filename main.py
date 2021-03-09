912f353e2555efd9c66f0db1d0759a3ea86937c1
mrozo2m <mrozo2m@interia.pl>
2021-03-09 21:34:09 +0100

    Added division into vowels and consonants

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ password = input('Podaj hasło ')
 print('\n\n\n\n\n')
 
 whell = [100, 150, 200, 250, 300, 350, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 5000, 'Bankrut', 'Bankrut']
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','ł','m','n','o','p','r','s','t','u','w','y','z','ą','ć','ń','ż','ź','x','ó']
+#alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','ł','m','n','o','p','r','s','t','u','w','y','z','ą','ć','ń','ż','ź','x','ó','ę']
+vowels = ['a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'ó', 'y']
+vowels_cost = 200
+consonants = ['b','c','d','f','g','h','j','k','l','ł','m','n','p','r','s','t','w','z','ć','ń','ż','ź','x']
 player_points = 0
 password_blind = ''
 for i in range(0,len(password)):
@@ -19,30 +22,51 @@ while password != password_blind:
     print('==========================================================')
     draw = random.choice(whell)
     print('Kręcisz kołem ...')
-    time.sleep(2)
+    time.sleep(1)
     print('koło zatrzymało się na:', draw)
     if draw == 'Bankrut':
         player_points = 0
         continue
     j = 0
     hit = 0
-    print('Podaj literę z hasła lub odgaduj hasło')
+    print('Podaj spółgłoskę z hasła lub odgaduj hasło')
     quess = input()
     if len(quess) > 1 and quess == password:
         print('Brawo ! Odgadnięte hasło to:', password)
         player_points = player_points + draw
         break
     else:
-        if quess in alphabet:
+        if quess in consonants:
             for i in password:
                 j = j + 1
                 if quess == i:
                     password_blind = password_blind[:j-1] + quess + password_blind[j:]
                     player_points = player_points + draw
                     hit = 1
-            alphabet.remove(quess)
+            consonants.remove(quess)
+            print('Czy chcesz wykupić samogłoskę t/n')
+            k = input()
+            if  k == 't':
+                print('\n==========================================================')
+                print("Kategoria:", category, '  Punkty:', player_points)
+                print("Hasło do odgadnięcia:", password_blind)
+                print('==========================================================')
+                print('Podaj samogłoskę z hasła')
+                quess = input()
+                j = 0
+                if quess in vowels:
+                    for i in password:
+                        j = j + 1
+                        if quess == i:
+                            password_blind = password_blind[:j - 1] + quess + password_blind[j:]
+                            player_points = player_points + draw
+                            hit = 1
+                    vowels.remove(quess)
+                    player_points = player_points - vowels_cost
+                elif len(quess) == 1:
+                    print("Samogłoska", quess, "już była użyta lub nie jest to samogłoska - wybierz inną nastepnym razem")
         elif len(quess) == 1:
-            print("Litera", quess, "już była użyta - wybierz inną")
+            print("Spółgłoska", quess, "już była użyta lub nie jest to spółgłoska - wybierz inną nastepnym razem")
         else:
             print("Nie prawidłowe hasło")
     if hit > 0:
